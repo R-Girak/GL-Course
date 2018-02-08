ef8ec5eeeb3281e59b05b87d0876c788f9f7a4f6
roman.girak <roman.girak@gmail.com>
2018-02-08 17:57:59 +0200

    2

---
+++ src/Problem107.java
@@ -0,0 +1,19 @@
+/**
+ * Created by roman.girak on 08/02/2018.
+ */
+public class Problem107 {
+//    Compact discs
+//    https://www.e-olymp.com/en/problems/107
+//
+//    Blank compact discs are on sale in packages of three kinds.
+//    A package with 100 disсs costs 100 UAH, one with 20 discs costs 30 UAH,
+//    and one disс costs 2 UAH. What minimal sum of money should you pay to buy n such disсs?
+//    Input
+//    The natural number n (n ≤ 1000).
+//    Output
+//    Print the minimal sum to buy n disсs.
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Problem923.java
@@ -0,0 +1,35 @@
+/**
+ * Created by roman.girak on 08/02/2018.
+ */
+public class Problem923 {
+//    Season
+//    https://www.e-olymp.com/en/problems/923
+//
+//    Determine the season name by the month number using the compound conditions.
+//    Input
+//    The number of the month.
+//    Output
+//    For spring months print Spring, for summer - Summer, for autumn - Autumn and for winter - Winter.
+
+    public static void main(String[] args) {
+        int month;
+        month = 6;
+
+        if (month >= 3 & month <= 5) {
+            System.out.println("Spring");
+        }
+        else if (month >= 6 & month <= 8) {
+            System.out.println("Summer");
+        }
+        else if (month >= 9 & month <= 11) {
+            System.out.println("Autumn");
+        }
+        else if (month == 12 || month == 1 || month == 2) {
+            System.out.println("Winter");
+        }
+        else {
+            System.out.println("No such month exists");
+        }
+
+    }
+}
